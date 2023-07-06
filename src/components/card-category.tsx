01161f7172629fe6ca445d139dba95fb7be44e26
Lucas Kahlil <kahlilpereira@gmail.com>
2023-07-06 19:24:21 -0300

    style: implement style card category

--- src/components/card-category.tsx
+++ src/components/card-category.tsx
@@ -1,8 +1,10 @@
 import { CategoryProps } from "../utils/types/request.ts"
 
+import '../style/components/card-category.sass'
+
 export default function CardCategory(category: CategoryProps) {
   return (
-    <div>
+    <div id="card-category">
         <img src={category.img} alt="imagem da categoria" />
         <p>{category.name}</p>
     </div>
