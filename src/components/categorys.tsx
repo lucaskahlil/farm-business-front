ff4ec2194492e0473dab7b72cd1a0fa314af11a3
Lucas Kahlil <kahlilpereira@gmail.com>
2023-07-07 20:22:43 -0300

    style: increment style on card category and section category

--- src/components/categorys.tsx
+++ src/components/categorys.tsx
@@ -1,18 +1,24 @@
 import { CategoryProps } from "../utils/types/request"
 import CardCategory from "./card-category"
+import '../style/components/categorys.sass'
 
 export default function Categorys() {
   
   const AllCategorys: CategoryProps[] = [
-    {id: "", img: "", title: ""},
-    {id: "", img: "", title: ""},
-    {id: "", img: "", title: ""},
+    {id: "img1", img: "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia26195/chacara-sitio-ou-fazenda-cursos-cpt.jpg", title: "Categoria"},
+    {id: "img2", img: "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia26195/chacara-sitio-ou-fazenda-cursos-cpt.jpg", title: "Categoria"},
+    {id: "img3", img: "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia26195/chacara-sitio-ou-fazenda-cursos-cpt.jpg", title: "Categoria"},
+    {id: "img4", img: "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia26195/chacara-sitio-ou-fazenda-cursos-cpt.jpg", title: "Categoria"},
+    {id: "img5", img: "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia26195/chacara-sitio-ou-fazenda-cursos-cpt.jpg", title: "Categoria"},
+    {id: "img6", img: "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia26195/chacara-sitio-ou-fazenda-cursos-cpt.jpg", title: "Categoria"},
+    {id: "img7", img: "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia26195/chacara-sitio-ou-fazenda-cursos-cpt.jpg", title: "Categoria"},
+    {id: "img8", img: "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia26195/chacara-sitio-ou-fazenda-cursos-cpt.jpg", title: "Categoria"}
   ]
 
   return (
-    <section>
+    <section id="category">
       <h2>Categorias no Agro</h2>
-      <div>
+      <div className="categorys-container">
         {AllCategorys.map((category) => (
           < CardCategory key={category.id} categoria={category}/>
         ))}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style/main.sass'
+import Categorys from './components/categorys'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    
+    <Categorys />
   </React.StrictMode>,
 )
